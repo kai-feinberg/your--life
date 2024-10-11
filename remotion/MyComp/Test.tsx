73789a9be74b22e2473a1cb1b21acd398e38d89c
Kai Feinberg <kaiwfeinberg@gmail.com>
2024-10-11 09:39:14 -0500

    audio and image sync

--- remotion/MyComp/Test.tsx
+++ remotion/MyComp/Test.tsx
@@ -6,27 +6,43 @@ import {
     useCurrentFrame,
     useVideoConfig,
     Img,
-    staticFile
+    staticFile,
+    Audio
 } from "remotion";
-import { CompositionProps } from "../../types/constants";
-import React from "react";
-
+import {getAudioDurationInSeconds} from "@remotion/media-utils";
+import React, { useEffect, useState } from "react";
 import { Animated } from "remotion-animated";
 
 
+export const Test = ({ images=["lebron1.jpg", "lebron2.jpg"] , title = "Default Title", mp3File= "30s-audio.mp3" }: { images?: string[]; title?: string; mp3File?: string }) => {
+    //check length of provided mp3
+    const [audioLength, setAudioLength] = useState(1); // audio length in frames
 
-export const Test = ({ title }: z.infer<typeof CompositionProps>) => {
+    useEffect(() => {
+        const getAudioDuration = async () => {
+            const audioLength = await getAudioDurationInSeconds(staticFile(mp3File))
+            setAudioLength(parseInt((audioLength*30).toString()));
+        }
 
+        getAudioDuration();
+    }, []);
+
+    console.log(parseInt((audioLength*30).toString()));
+
+    const imageLength = audioLength/images.length;
+
+    //divide by number of images for the section and convert to frames
     return (
         <AbsoluteFill>
-            <Sequence durationInFrames={900}>
-                <AbsoluteFill className="justify-center items-center">
-                    <Img src={staticFile("lebron1.jpg")} />
-                </AbsoluteFill>
-            </Sequence>
-            <Sequence from={900}>
-                <Img src={staticFile("lebron1.jpg")} />
-            </Sequence>
+            <Audio src={staticFile(mp3File)} />
+            {images.map((path, index) => (
+                <Sequence key={index} durationInFrames={imageLength} from={index * imageLength}>
+                    <AbsoluteFill className="justify-center items-center">
+
+                        <Img src={staticFile(path)} />
+                    </AbsoluteFill>
+                </Sequence>
+            ))}
         </AbsoluteFill>
     );
 };
